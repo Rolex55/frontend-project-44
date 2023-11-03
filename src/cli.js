66e9a285af49a107f899bfb2809d1f733e269540
Ralina Salikhova <ralina.salikhova48@gmail.com>
2023-11-03 13:29:28 +0300

    add eslint and codeclimate

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
 
 const greetingName = () => {
-  const name = readlineSync.question("May I have your name? ");
+  const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
 };
 
-export { greetingName };
+export default greetingName;
