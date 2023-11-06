0beab9af5c7aabd31e631753a31887a2a1425c2c
Ralina Salikhova <ralina.salikhova48@gmail.com>
2023-11-06 13:19:03 +0300

    add brain-progression.js

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 https://asciinema.org/a/xTfQp5Yx0bZxTtThyYu1ptEwo
 https://asciinema.org/a/rJaMamWd6rwwYESZxsTCaJmrg
 https://asciinema.org/a/ZkvTSCgCuzXj22CEcICFaM7L6
+https://asciinema.org/a/zzKf1hSXoepr20aFZ7Du7xSb6
---
+++ src/games/brain-progression.js
@@ -0,0 +1,32 @@
+import comparingExpressionAnswer from "../index.js";
+
+const gettingProgressionWithHiddenNumber = () => {
+  const startNumber = Math.floor(Math.random() * 20);
+  const step = Math.floor(Math.random() * 10 + 1);
+  let progression = [];
+  let number;
+  for (let i = 0; i < 10; i += 1) {
+    number = startNumber + step * i;
+    progression.push(number);
+  }
+  const hiddenIndex = Math.floor(Math.random() * 10);
+  const hiddenNumber = progression.splice(hiddenIndex, 1, "..");
+  progression = progression.join(" ");
+  return [progression, hiddenNumber];
+};
+
+const brainProgressionGame = () => {
+  const expressions = [];
+  const correctAnswers = [];
+  let progression;
+  let hiddenNumber;
+  for (let j = 1; j <= 3; j += 1) {
+    [progression, hiddenNumber] = gettingProgressionWithHiddenNumber();
+    expressions.push(progression);
+    correctAnswers.push(hiddenNumber.toString());
+  }
+  const mainQuestionOfGame = "What number is missing in the progression?";
+  comparingExpressionAnswer(expressions, correctAnswers, mainQuestionOfGame);
+};
+
+export default brainProgressionGame;
